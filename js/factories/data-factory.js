c392101e6c9307f00870fa12c3b726be78d4cdf1
Laetitia C <7393579-Tysscia@users.noreply.gitlab.com>
2022-03-16 03:30:07 +0100

    mise en place du systeme de recherche

--- js/factories/data-factory.js
+++ js/factories/data-factory.js
@@ -1,38 +1,3 @@
-// class Data {
-//     constructor(id, name, servings, ingredients, ingredient, time, description, appliance, ustensils){
-//         this.id = id;
-//         this.name = name; 
-//         this.servings = servings;
-//           this.time = time;
-//         this.description = description;
-//         this.appliance = appliance;
-//         this.ustensils = ustensils;
-//     }
-
-//     showRecipes() {
-//        return `
-//         <div class="card mb-5" aria-hidden="true">
-//             <img src="../../public/assets/image-recette.jpg" class="card-img-top" alt="...">
-//             <div class="card-body">
-//                 <h5 class="card-title placeholder-glow d-flex justify-content-between">
-//                     <span class="col-10">${this.name}</span>
-//                     <span class="d-flex align-items-center"><i class="card-icon-time mx-2"></i>${this.time}</span>   
-//                 </h5>
-//                 <p class="card-text placeholder-glow d-flex">
-//                     <span class="col-6">
-//                     <ul class="ingredients-list">y
-//                     <li>${this.ingredient}</li>
-//                     </ul>
-//                     </span>
-//                     <span class="col-6 card-description">${this.description}</span>
-//                 </p>
-//             </div>
-//         </div> 
-//         `
-//     }
-// }      this.ingredients = ingredients;
-//         this.ingredient = ingredient;
-
 
 function recipesFactory (data) {
     const { id, name, servings, ingredients, time, description, appliance, ustensils } = data
@@ -42,39 +7,30 @@ function recipesFactory (data) {
         let ingredientItem
         let quantityItem
         let unitItem
-        let ingredientString = [];
-
-        for(const items in ingredients){
-            console.log(ingredients[items])
-            ingredientItem = ingredients[items]
-            quantityItem = ingredients[items].quantity
-            unitItem = ingredients[items].unit
+        let ingredientTag = ``
             
-            for ( const ingredient in ingredientItem ){
-                console.log(ingredient)
-                ingredientString.push(ingredient)
-            }
+        for (const item of ingredients ){
+            ingredientItem = item
+            quantityItem = item.quantity
+            unitItem = item.unit
+            ingredientTag += `<li><strong>${item.ingredient ?? ''}:</strong> ${item.quantity ?? ''} ${item.unit ?? ''}</li>`
         }
-
         return `
         <div class="card mb-5" aria-hidden="true">
             <img src="../../public/assets/image-recette.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title placeholder-glow d-flex justify-content-between">
                     <span class="col-10">${name}</span>
-                    <span class="d-flex align-items-center"><i class="card-icon-time mx-2"></i>${time}</span>   
+                    <span class="d-flex align-items-center"><i class="card-icon-time mx-2"></i>${time}</span>
                 </h5>
-                <p class="card-text placeholder-glow d-flex">
-                    <span class="col-6">
-                        <ul class="ingredients-list">
-                            <li>Ingredients: ${ingredientString.map(ing => ing.ingredient)}</li>
-                            <li>${quantityItem}`+` ${unitItem}</li>
-                        </ul>
-                    </span>
+                <div class="d-flex justify-content-between">
+                    <ul class="ingredients-list" id="ingredients-list">
+                    ${ingredientTag}
+                    </ul>
                     <span class="col-6 card-description">${description}</span>
-                </p>
+                </div>
             </div>
-        </div> 
+        </div>
         `                
     }
 
--- js/search.js
+++ js/search.js
@@ -17,6 +17,59 @@ async function dataRecipe () {
     return recipesData
 }
 
+function searchRecipe (data){
+    let itemsName = []; 
+    let itemsIngredients;
+    let itemsDescription;
+
+    for(const items of data){
+        itemsName.push(items.name)
+        itemsIngredients = items.ingredients
+        itemsDescription = items.description
+        console.log(itemsName)
+    }
+
+    const searchInput = document.querySelector('.input-search');
+    let recipesList = document.getElementById('bloc-recipe');
+
+    searchInput.onkeyup = function () {
+        const searchInputValue = searchInput.value;
+        
+        const searchByTitle = itemsName.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase()));
+        console.log(searchByTitle)
+        const searchByIngredients = itemsIngredients.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase())); 
+        const searchByDescription = itemsDescription.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase())); 
+        let suggestions = ''; 
+
+        if(searchInputValue != ""){
+            searchByTitle.forEach(nameItem => {
+                recipesList.innerHTML = `
+                <div class="card mb-5" aria-hidden="true">
+                    <img src="../../public/assets/image-recette.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title placeholder-glow d-flex justify-content-between">
+                            <span class="col-10">${nameItem}</span>
+                            <span class="d-flex align-items-center"><i class="card-icon-time mx-2"></i>${time}</span>
+                        </h5>
+                        <div class="d-flex justify-content-between">
+                            <ul class="ingredients-list" id="ingredients-list">
+                            ${ingredientTag}
+                            </ul>
+                            <span class="col-6 card-description">${description}</span>
+                        </div>
+                    </div>
+                </div>
+                ` 
+            })
+        }
+        
+        document.getElementById('search-suggestion').innerHTML = `${suggestions}` 
+
+    }
+
+
+}
+
 
 
 async function displayRecipes(recipes){
@@ -33,5 +86,6 @@ async function displayRecipes(recipes){
 async function init (){
     const recipes = await dataRecipe()
     displayRecipes(recipes)
+    searchRecipe(recipes)
 }
 init();

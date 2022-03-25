596eee3adfe02193143e19bfb8f1716cae724375
Laetitia C <7393579-Tysscia@users.noreply.gitlab.com>
2022-03-25 17:22:50 +0100

    add research by name

--- js/search.js
+++ js/search.js
@@ -12,21 +12,16 @@ async function getRecipes() {
 
 async function dataRecipe () {
     const data = await getRecipes();
-    console.log(data) 
+    // console.log(data) 
     const recipesData = data.recipes
     return recipesData
 }
 
 function searchRecipe (data){
-    let itemsName = []; 
-    let itemsIngredients;
-    let itemsDescription;
+    let itemsRecipes = [];
 
     for(const items of data){
-        itemsName.push(items.name)
-        itemsIngredients = items.ingredients
-        itemsDescription = items.description
-        console.log(itemsName)
+        itemsRecipes.push(items);
     }
 
     const searchInput = document.querySelector('.input-search');
@@ -34,33 +29,33 @@ function searchRecipe (data){
 
     searchInput.onkeyup = function () {
         const searchInputValue = searchInput.value;
-        
-        const searchByTitle = itemsName.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase()));
-        console.log(searchByTitle)
-        const searchByIngredients = itemsIngredients.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase())); 
-        const searchByDescription = itemsDescription.filter(item => item.toLowerCase().includes(searchInputValue.toLowerCase())); 
+
+        if (searchInputValue.length < 2) {
+            return
+        }
+
+        const foundRecipes = itemsRecipes.filter(item => item.name.toLowerCase().includes(searchInputValue.toLowerCase()));
+        // console.log(foundRecipes)
+        // const searchByIngredients = itemsRecipes.filter(item => item.ingredients[0].toLowerCase().includes(searchInputValue.toLowerCase()));
+        const searchByIngredients = itemsRecipes.filter(item => item.ingredients.toString().toLowerCase().includes(searchInputValue.toLowerCase()));
+        console.log(searchByIngredients)
+
+        const descriptionList = itemsRecipes.filter(item => item.description.toLowerCase().includes(searchInputValue.toLowerCase()));
+        // console.log(descriptionList)
         let suggestions = ''; 
 
         if(searchInputValue != ""){
-            searchByTitle.forEach(nameItem => {
-                recipesList.innerHTML = `
-                <div class="card mb-5" aria-hidden="true">
-                    <img src="../../public/assets/image-recette.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title placeholder-glow d-flex justify-content-between">
-                            <span class="col-10">${nameItem}</span>
-                            <span class="d-flex align-items-center"><i class="card-icon-time mx-2"></i>${time}</span>
-                        </h5>
-                        <div class="d-flex justify-content-between">
-                            <ul class="ingredients-list" id="ingredients-list">
-                            ${ingredientTag}
-                            </ul>
-                            <span class="col-6 card-description">${description}</span>
-                        </div>
-                    </div>
-                </div>
-                ` 
+            recipesList.innerHTML = ""
+            foundRecipes.forEach(item => {
+                const recipesModel = recipesFactory(item)
+                const recipesDOM = recipesModel.showRecipes(item)
+                recipesList.innerHTML += recipesDOM
             })
+            // descriptionList.forEach(item => {
+            //     const recipesModel = recipesFactory(item)
+            //     const recipesDOM = recipesModel.showRecipes(item)
+            //     recipesList.innerHTML += recipesDOM
+            // })
         }
         
         document.getElementById('search-suggestion').innerHTML = `${suggestions}` 
@@ -70,7 +65,15 @@ function searchRecipe (data){
 
 }
 
+// async function displaySearchRecipes(recipes){
+//     const recipeTag = document.getElementById('bloc-recipe')
+//         recipes.forEach((recipe) => {
+//          const recipesModel = recipesFactory(recipe)
+//          const recipesDOM = recipesModel.search(recipe)
+//          recipeTag.innerHTML += recipesDOM
+//      });
 
+// };
 
 async function displayRecipes(recipes){
     const recipeTag = document.getElementById('bloc-recipe')
@@ -86,6 +89,7 @@ async function displayRecipes(recipes){
 async function init (){
     const recipes = await dataRecipe()
     displayRecipes(recipes)
+    // displaySearchRecipes(recipes)
     searchRecipe(recipes)
 }
 init();
